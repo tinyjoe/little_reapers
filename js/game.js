48c0cdc3de958a473db63c23f973d9590a219cd1
tinajoelly <tinaandrea.joelly2@edu.fh-kaernten.ac.at>
2025-01-29 22:10:52 +0100

    help dialog and settings buttons

--- js/game.js
+++ js/game.js
@@ -14,27 +14,62 @@ function init() {
   world = new World(canvas, keyboard);
 }
 
+function showHelpDialog() {
+  let overlay = document.getElementById("info-dialog");
+  overlay.classList.remove("hidden");
+  overlay.innerHTML += renderHelpDialog();
+}
+
+function closeHelpDialog() {
+  let overlay = document.getElementById("info-dialog");
+  overlay.classList.add("hidden");
+  overlay.innerHTML = "";
+}
+
+function renderHelpDialog() {
+  return `<div class="overlayContent">
+        <div class="infoDialog">
+          <h3>How to play</h3>
+          <div class="keyInfoRow">
+            <div class="infoContent">
+              <img
+                class="moveButton"
+                src="./img/elements/buttons_move_reaper.png"
+              />
+              <p class="infoText">Move Reaper</p>
+            </div>
+            <div class="infoContent">
+              <img class="keyButton" src="./img/elements/button_space.png" />
+              <p class="infoText">Jump</p>
+            </div>
+            <div class="infoContent">
+              <img class="keyButton" src="./img/elements/button_shift.png" />
+              <p class="infoText">Throw bottles</p>
+            </div>
+          </div>
+        </div>
+        <button onclick="closeHelpDialog()" class="closeButton">
+        </button>
+      </div>
+      `;
+}
+
 document.addEventListener("keydown", (event) => {
   if (event.keyCode === 39) {
     keyboard.RIGHT = true;
   }
-
   if (event.keyCode === 37) {
     keyboard.LEFT = true;
   }
-
   if (event.keyCode === 38) {
     keyboard.UP = true;
   }
-
   if (event.keyCode === 40) {
     keyboard.DOWN = true;
   }
-
   if (event.keyCode === 32) {
     keyboard.JUMP = true;
   }
-
   if (event.keyCode === 16) {
     keyboard.THROW = true;
   }
